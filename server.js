56b6e5783bec54e3797487fddc4d527077f62fda
raniafathallah <raniaahmed7000@gmail.com>
2022-12-06 00:48:29 +0200

    add-tourControlle-user/tour model-erro-handling-express&global

--- server.js
+++ server.js
@@ -4,10 +4,35 @@ const App =require('./app');
 
 // App.use(express.json())
 dotenv.config();
- 
 
-// SERVER STARTER
-const port = process.env.PORT || 8000;
-const server = App.listen(port, () => {
-    console.log(`<--- App running on ${process.env.NODE_ENV} (Port: ${port}) --->`);
+process.on('uncaughtException', err => {
+    console.log(err.name, err.message);
+
+    console.log('Shutting down program...');
+    process.exit(1);
 });
+ 
+const port = process.env.PORT || 8000;
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('DB connection stablished!');
+    App.listen(port, () => {
+        console.log(`<--- App running on ${process.env.NODE_ENV} (Port: ${port}) --->`);
+    });
+    
+}
+);
+
+
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+
+    console.log('Shutting down program...');
+    server.close(() => {
+        process.exit(1);
+    });
+});
+// SERVER STARTER
